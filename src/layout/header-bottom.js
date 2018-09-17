b41bd77eeae190e317b221ad0483c6a238f36c9a
Jirffey Gabuya <boyhimas@gmail.com>
2018-09-18 02:49:14 +0800

    Cleanup

--- src/layout/header-bottom.js
+++ src/layout/header-bottom.js
@@ -1,38 +1,12 @@
 // global $
-import { detect } from 'detect-browser';
 import Layout from '../lib/layout';
 
 export default class HeaderBottom extends Layout {
-    constructor() {
-        super();
-
-        this.$el = $('#ctl00_HeaderBottom_pnlContentContainerData');
-        this.$categoriesContainer = $('#ctl00_HeaderBottom_Content_115_pnlContentData');
-        this.$searchContainer = $('#ctl00_HeaderBottom_Content_116_pnlContentData');
-
-        this.$movableElements = [
-            this.$categoriesContainer,
-            this.$searchContainer
-        ]
-    }
-
-    cleanUpSearchBar() {
-        this.$searchContainer.html(this.$searchContainer.html().replace('Search:', ''));
-    }
-
-    addSearchbarFirefoxClass() {
-        const browser = detect();
-        const firefox = 'firefox';
-
-        if (browser && browser.name === firefox) {
-            this.$searchContainer.find('input').addClass(firefox);
-        }
+    constructor(props) {
+        super(props);
     }
 
     render() {
         super.render();
-
-        this.cleanUpSearchBar();
-        this.addSearchbarFirefoxClass();
     }
 }
--- src/layout/header-top.js
+++ src/layout/header-top.js
@@ -2,20 +2,8 @@
 import Layout from '../lib/layout';
 
 export default class HeaderTop extends Layout {
-    constructor() {
-        super();
-
-        // cache selectors
-        this.$el = $('#ctl00_HeaderTop_pnlContentContainerData');
-        this.$mainMenuContainer = $('#ctl00_HeaderTop_Content_23_pnlContentContainerData');
-        this.$loginMenuContainer = $('#ctl00_HeaderTop_Content_3_Content_25_pnlContentData');
-
-        this.$movableElements = [
-            this.$mainMenuContainer,
-            this.$loginMenuContainer,
-            this.$el.find('.socials'),
-            this.$el.find('.welcome-msg')
-        ]
+    constructor(props) {
+        super(props);
     }
 
     render() {
