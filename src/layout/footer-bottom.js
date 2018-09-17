47af7b3098fd7d7fbbd13d84c0683fcef8b5a632
Jirffey Gabuya <boyhimas@gmail.com>
2018-09-18 02:49:00 +0800

    Additional layout classes

---
+++ src/layout/footer-bottom.js
@@ -0,0 +1,12 @@
+// global $
+import Layout from '../lib/layout';
+
+export default class FooterBottom extends Layout {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        super.render();
+    }
+}
---
+++ src/layout/footer-middle.js
@@ -0,0 +1,12 @@
+// global $
+import Layout from '../lib/layout';
+
+export default class FooterMiddle extends Layout {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        super.render();
+    }
+}
