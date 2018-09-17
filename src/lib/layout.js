0c6f0d070e9fa541484807563fe9f28b91ed489c
Jirffey Gabuya <boyhimas@gmail.com>
2018-09-18 03:36:31 +0800

    Add diagnostic message, remove semi-colons

--- src/lib/layout.js
+++ src/lib/layout.js
@@ -1,30 +1,32 @@
 // global $
-import config from '../config';
-import createElement from './utils/create-element';
+import config from '../config'
+import createElement from './utils/create-element'
 
 class Layout {
     constructor(props) {
-        this.$el = props.$el;
-        this.$movableElements = props.$movableElements;
-        this.isFluid = props.isFluid;
+        this.$el = props.$el
+        this.$movableElements = props.$movableElements
+        this.isFluid = props.isFluid
     }
 
     addContainer() {
-        const containerClassname = (this.isFluid)? config.CONTAINER_FLUID_CLASSNAME : config.CONTAINER_CLASSNAME;
+        const containerClassname = (this.isFluid)? config.CONTAINER_FLUID_CLASSNAME : config.CONTAINER_CLASSNAME
 
-        this.$container = createElement('div', containerClassname);
-        this.$el.prepend(this.$container);
+        this.$container = createElement('div', containerClassname)
+        this.$el.prepend(this.$container)
     }
 
     moveElementsToContainer() {
         this.$movableElements.forEach(($item) => {
-            $item.appendTo(this.$container);
-        });
+            $item.appendTo(this.$container)
+        })
     }
 
     render() {
-        this.addContainer();
-        this.moveElementsToContainer();
+        this.addContainer()
+        this.moveElementsToContainer()
+
+        console.log(`${this.constructor.name} has finished rendering.`)
     }
 }
 
