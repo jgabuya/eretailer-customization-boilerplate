644c8a6e03ea4eaa2d5ae91d4b0942a3895d9daf
Jirffey Gabuya <boyhimas@gmail.com>
2018-09-18 02:31:57 +0800

    Add config, constant key-values

--- src/config.js
+++ src/config.js
@@ -1,4 +1,127 @@
 export default {
-    CONTAINER_CLASSNAME: 'container',
-    CONTAINER_FLUID_CLASSNAME: 'container-fluid'
+    HEADER_TOP_PROPS: {
+        $el: $(''),
+        $movableElements: [],
+        isFluid = false
+    },
+
+    HEADER_MIDDLE_PROPS: {
+        $el: $(''),
+        $movableElements: [],
+        isFluid = false
+    },
+
+    HEADER_BOTTOM_PROPS: {
+        $el: $(''),
+        $movableElements: [],
+        isFluid = false
+    },
+
+    FOOTER_TOP_PROPS: {
+        $el: $(''),
+        $movableElements: [],
+        isFluid = false
+    },
+    
+    FOOTER_MIDDLE_PROPS: {
+        $el: $(''),
+        $movableElements: [],
+        isFluid = false
+    },
+
+    FOOTER_BOTTOM_PROPS: {
+        $el: $(''),
+        $movableElements: [],
+        isFluid = false
+    },
+
+    CONTENT_HOME_PROPS: {
+        $el: $(''),
+        $movableElements: [],
+        isFluid = false
+    },
+
+    CONTENT_LOGIN_PROPS: {
+        $el: $(''),
+        $movableElements: [],
+        isFluid = false
+    },
+
+    CONTENT_REGISTER_PROPS: {
+        $el: $(''),
+        $movableElements: [],
+        isFluid = false
+    },
+
+    CONTENT_ALL_CATEGORIES_PROPS: {
+        $el: $(''),
+        $movableElements: [],
+        isFluid = false
+    },
+
+    CONTENT_SHOPPING_CART_PROPS: {
+        $el: $(''),
+        $movableElements: [],
+        isFluid = false
+    },
+
+    CONTENT_WISHLIST_PROPS: {
+        $el: $(''),
+        $movableElements: [],
+        isFluid = false
+    },
+
+    CONTENT_GUEST_CHECKOUT_PROPS: {
+        $el: $(''),
+        $movableElements: [],
+        isFluid = false
+    },
+
+    CONTENT_CHECKOUT_PROPS: {
+        $el: $(''),
+        $movableElements: [],
+        isFluid = false
+    },
+
+    CONTENT_MY_ACCOUNT_PROPS: {
+        $el: $(''),
+        $movableElements: [],
+        isFluid = false
+    },
+
+    CONTENT_CONTACT_FORM_PROPS: {
+        $el: $(''),
+        $movableElements: [],
+        isFluid = false
+    },
+
+    CONTENT_CATEGORY_PROPS: {
+        $el: $(''),
+        $movableElements: [],
+        isFluid = false
+    },
+
+    CONTENT_PRODUCT_PROPS: {
+        $el: $(''),
+        $movableElements: [],
+        isFluid = false
+    },
+
+    CONTENT_SEARCH_PROPS: {
+        $el: $(''),
+        $movableElements: [],
+        isFluid = false
+    },
+
+    CONTENT_INFO_PAGE_PROPS: {
+        $el: $(''),
+        $movableElements: [],
+        isFluid = false
+    },
+
+    CONTENT_DEFAULT_PROPS: {
+        $el: $(''),
+        $movableElements: [],
+        isFluid = false
+    },
 }
