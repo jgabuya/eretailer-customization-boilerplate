88837cb2ed083d701d4f5b92d562660da340a2ef
Jirffey Gabuya <boyhimas@gmail.com>
2018-09-24 19:26:52 +0800

    #3: Add fix

--- src/app.js
+++ src/app.js
@@ -1,18 +1,27 @@
 // global $
 import di from './di-registry'
-import constants from './constants'
+import {
+    GLOBAL,
+    HEADER_TOP,
+    HEADER_MIDDLE,
+    HEADER_BOTTOM,
+    FOOTER_TOP,
+    FOOTER_MIDDLE,
+    FOOTER_BOTTOM,
+    CONTENT
+} from './constants'
 
 // import styles
 import './styles/app.scss'
 
 // render customizations
 $(document).ready(() => {
-    di.get(constants.GLOBAL).render()
-    di.get(constants.HEADER_TOP).render()
-    di.get(constants.HEADER_MIDDLE).render()
-    di.get(constants.HEADER_BOTTOM).render()
-    di.get(constants.FOOTER_TOP).render()
-    di.get(constants.FOOTER_MIDDLE).render()
-    di.get(constants.FOOTER_BOTTOM).render()
-    di.get(constants.CONTENT).render()
+    di.get(GLOBAL).render()
+    di.get(HEADER_TOP).render()
+    di.get(HEADER_MIDDLE).render()
+    di.get(HEADER_BOTTOM).render()
+    di.get(FOOTER_TOP).render()
+    di.get(FOOTER_MIDDLE).render()
+    di.get(FOOTER_BOTTOM).render()
+    di.get(CONTENT).render()
 })
